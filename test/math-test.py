a780e20234182270f87c08931372b08a5a9bfd3e
Patrick <patrickwolf1986@gmail.com>
2017-09-27 14:51:36 +0200

    test1

--- test/math-test.py
+++ test/math-test.py
@@ -4,4 +4,4 @@ from src.math import Math
 class MathTest(unittest.TestCase):
 	def test_addition(self):
 	# Make test fail
-		self.assertEqual(Math.addition(3, 4), 7)
+		self.assertEqual(Math.addition(3, 4), 8)
